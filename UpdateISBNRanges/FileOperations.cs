e05649101d1939647258edded09580f3fe846f9e
Olli Eskola <olli.eskola@csc.fi>
2021-08-07 00:05:45 +0300

    first commit

---
+++ UpdateISBNRanges/FileOperations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace UpdateISBNRanges
+{
+    class FileOperations
+    {
+
+        // delete file in path if exists
+        public void deleteFile(string path)
+        {
+            if (File.Exists(@path))
+            {
+                File.Delete(@path);
+            }
+
+        }
+
+
+        // get file from API
+        public void getFileFromAPI(string path)
+        {
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            WebClient client = new WebClient();
+
+            string url = "https://www.isbn-international.org/export_rangemessage.xml";
+
+            client.DownloadFile(url, @path);
+
+        }
+
+
+        public void Remove_DTD_From_XML(string path)
+        {
+            try
+            {
+                XmlDocument XDoc = new XmlDocument();
+                XDoc.Load(path);
+                XmlDocumentType XDType = XDoc.DocumentType;
+                XDoc.RemoveChild(XDType);
+                XDoc.Save(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+
+}
---
+++ UpdateISBNRanges/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace UpdateISBNRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //////////////////////////////////////////////
+            // There is one parameter given to program  //
+            // 1. args[0] = path to file                //
+            ////////////////////////////////////////////////////////////////////////
+
+            if (args.Length != 1)
+            {
+                Console.Write("amount of arguments is wrong");
+            }
+
+            else
+            {
+
+                FileOperations fileOperations = new FileOperations();
+
+                // path to file which is first deleted (if exists) and after that get from ISBN api
+                string pathToFile = args[0];
+
+                // destroy the old file if exist
+                fileOperations.deleteFile(pathToFile);
+
+                // get file from the ISBN API
+                fileOperations.getFileFromAPI(pathToFile);
+
+                // remove DTD declarations at the beginning of xml file
+                fileOperations.Remove_DTD_From_XML(pathToFile);
+
+            }
+
+        }
+
+    }
+
+}
